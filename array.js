c4308c2b4a55a210ac8c60d1cbc9b3964d37e998
Manav Sharma <70696458+ManavSharma03@users.noreply.github.com>
2024-04-07 17:39:10 +0530

    subarray with sum k

--- array.js
+++ array.js
@@ -14,3 +14,23 @@ for (let i = 1; i < arr.length; i++) {
 // Example usage:
 const array = [5, 2, 10, 8, 3];
 const secondLargest = findSecondLargest(array);
+
+
+// smallest sub array with sum k
+function smallestSubarraySumK(arr, k)
+{
+    let result = Number.MAX_SAFE_INTEGER;
+ 
+    for (let i = 0; i < arr.length; ++i) {
+        let sum = 0;
+        for (let j = i; j < arr.length; j++) {
+            sum += arr[j];
+            if (sum == k) {
+                result = Math.min(result, (j - i + 1));
+            }
+        }
+    }
+ 
+    // Return result
+    return result;
+}
