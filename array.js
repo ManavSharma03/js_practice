16661e7c19753c9deb8b36eebf8c480ecab963f2
ManavSharma03 <mmanavssharma804@gmail.com>
2024-04-10 18:36:52 +0530

    fix: formatting file

--- array.js
+++ array.js
@@ -1,7 +1,7 @@
 function findSecondLargest(arr) {
   let largest = arr[0];
   let secondLargest = -Infinity;
-for (let i = 1; i < arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     if (arr[i] > largest) {
       secondLargest = largest;
       largest = arr[i];
@@ -15,22 +15,20 @@ for (let i = 1; i < arr.length; i++) {
 const array = [5, 2, 10, 8, 3];
 const secondLargest = findSecondLargest(array);
 
-
 // smallest sub array with sum k
-function smallestSubarraySumK(arr, k)
-{
-    let result = Number.MAX_SAFE_INTEGER;
- 
-    for (let i = 0; i < arr.length; ++i) {
-        let sum = 0;
-        for (let j = i; j < arr.length; j++) {
-            sum += arr[j];
-            if (sum == k) {
-                result = Math.min(result, (j - i + 1));
-            }
-        }
+function smallestSubarraySumK(arr, k) {
+  let result = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 0; i < arr.length; ++i) {
+    let sum = 0;
+    for (let j = i; j < arr.length; j++) {
+      sum += arr[j];
+      if (sum == k) {
+        result = Math.min(result, j - i + 1);
+      }
     }
- 
-    // Return result
-    return result;
+  }
+
+  // Return result
+  return result;
 }
