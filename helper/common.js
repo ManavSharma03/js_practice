d3d2657f0b4cd2033e1baf9ded672352da657086
ManavSharma03 <mmanavssharma804@gmail.com>
2024-04-11 11:01:25 +0530

    fix: longest substring helper function

---
+++ helper/common.js
@@ -0,0 +1,21 @@
+export const getCharacterOccurrence = (s) => {
+  const charCount = {};
+  for (let index = 0; index < s.length; index++) {
+    const element = s[index];
+    charCount[element] = charCount[element] ? charCount[element] + 1 : 1;
+  }
+
+  return charCount;
+};
+
+export const getMaxOccurrenceCharAndCOunt = (s) => {
+  const charOccurrence = getCharacterOccurrence(s);
+
+  return Object.entries(charOccurrence).reduce(
+    (result, [char, charOccurrence]) => {
+      return charOccurrence > result.maxCount
+        ? { char, maxCount: charOccurrence }
+        : result;
+    }
+  );
+};
