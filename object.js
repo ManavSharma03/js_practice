2b87e122e571883de38133446536feac0a29c7c1
Manav Sharma <70696458+ManavSharma03@users.noreply.github.com>
2024-04-14 22:18:05 +0530

    Update object.js

--- object.js
+++ object.js
@@ -1,3 +1,14 @@
 // practice problems for objecs
 
 // for in loop is for objects 
+
+
+export const getCharacterOccurrence = (s) => {
+  const charCount = {};
+  for (let index = 0; index < s.length; index++) {
+    const element = s[index];
+    charCount[element] = charCount[element] ? charCount[element] + 1 : 1;
+  }
+
+  return charCount;
+};
